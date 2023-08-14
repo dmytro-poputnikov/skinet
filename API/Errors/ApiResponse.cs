e5fa1cc4ef7bd9172c49b5b4111f5051dbf4b199
dmytro-poputnikov <dpoputnikov@gmail.com>
2023-08-15 00:11:00 +0200

    Error handling

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request",
+                401 => "You are not authorized",
+                404 => "A resource was not founded",
+                500 => "A Server error",
+                _ => null
+            };
+        }
+    }
+}
